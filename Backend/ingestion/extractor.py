677dbd8d0d63838044111f6631219c9e43b38f11
Shivansh Sinha <shivanshofc@gmail.com>
2026-02-21 19:40:48 +0530

    added pdf_extraction

--- Backend/ingestion/extractor.py
+++ Backend/ingestion/extractor.py
@@ -5,4 +5,13 @@ def _extract_txt(path: str) -> str:
         return f.read()
 
 def _extract_pdf(path: str) -> str:
-    pass
+    text =''
+    with pdfplumber.open(path) as pdf:
+        for page in pdf.pages:
+            try:
+                page_text = page.extract_text()
+                if page_text:
+                    text += page_text + "\n"
+            except Exception as e:
+                print("Pdf had an error: ", e)
+    return text
